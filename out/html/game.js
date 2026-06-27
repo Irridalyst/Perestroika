f1c239683c36b257bbb5dd776ad0ab83106fb7a1
Irridalyst <mroblan2005@gmail.com>
2026-06-26 23:15:59 -0400

    music tests

--- out/html/game.js
+++ out/html/game.js
@@ -262,7 +262,7 @@
 window._musicPlaylist = [
     { name: "Wide is my Motherland", src: "music/1987_1989/WideIsMyMotherland.mp3" },
     { name: "WDTMB", src: "music/1987_1989/WDTMB.mp3" },
-    { name: "Aviamarch", src: "Aviamarch.mp3" }
+    { name: "Aviamarch", src: "music/1987_1989/Aviamarch.mp3" }
 ];
 window._musicIndex = 0;
 window._musicAudio = new Audio();
